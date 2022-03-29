7c85a5eb54881a762eeaebe7f1554ab66bba9f95
Blender20 <rayyazdi@outlook.com>
2022-03-28 21:27:44 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert('Thi is a test')
+function deleteMe(element) {
+  element.remove();
+}
+
+function call(element){
+  alert("Ninja was liked")
+}
+
+function login(element,innerText){
+  element.innerText ="Logout"
+}
